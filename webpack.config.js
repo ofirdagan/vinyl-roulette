1b914717e35969a173e38f38fe36002fa436bf97
Ofir Dagan <ofirdagan@gmail.com>
2023-04-01 20:23:01 +0300

    build script

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ const stylesHandler = 'style-loader';
 const config = {
     entry: './src/index.ts',
     output: {
-        path: path.resolve(__dirname, 'docs'),
+        path: path.resolve(__dirname, 'dist'),
     },
     devServer: {
         open: true,
